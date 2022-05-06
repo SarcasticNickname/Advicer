715e42041b304fd08a337045f370d1bbf3617e5c
SarcasticNickname <78605109+SarcasticNickname@users.noreply.github.com>
2022-05-06 03:59:14 +0300

    First Android project

---
+++ app/src/main/java/com/example/beeradvicer/FindBeerActivity.java
@@ -0,0 +1,20 @@
+package com.example.beeradvicer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FindBeerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+
+    public void onClickFindBeer(View view) {
+
+    }
+}
